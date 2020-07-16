7be704f9dfe44ba0302ab376ef679c2d4e1f6637
Julio Chaile <juliochaile96@gmail.com>
2020-07-16 11:06:14 -0300

    ahora si

---
+++ assets/map/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="floor" tilewidth="16" tileheight="16" tilecount="160" columns="16">
+ <image source="dungeon.png" trans="b8b8b8" width="256" height="160"/>
+</tileset>
---
+++ assets/map/wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="wall" tilewidth="16" tileheight="16" tilecount="160" columns="16">
+ <image source="dungeon.png" trans="000000" width="256" height="160"/>
+</tileset>

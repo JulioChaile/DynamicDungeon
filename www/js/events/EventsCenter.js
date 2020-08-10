8c48f60f176eb6d36057171e2c73ac53250ab224
Julio <juliochaile96@gmail.com>
2020-08-10 10:30:56 -0300

    emitter fix

--- www/js/events/EventsCenter.js
+++ www/js/events/EventsCenter.js
@@ -1,4 +1,8 @@
 // Emisor de eventos, importar y usar para emitir y escuchar eventos entre diferentes complementos
-var emitter = new Phaser.EventEmitter();
+var emitter;
+if (!window.global_emitter) {
+    window.global_emitter =  new Phaser.EventEmitter()
+} 
+emitter = window.global_emitter
 
 export default emitter
